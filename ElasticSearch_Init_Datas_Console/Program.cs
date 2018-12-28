ed7ea0e22303e88717132addef1d52c6eb5f9c96
tangname <249195807@qq.com>
2018-12-28 11:10:55 +0800

    添加项目文件。

---
+++ ElasticSearch_Init_Datas_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElasticSearch_Init_Datas_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
